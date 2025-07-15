abb6971c7e09a84e67d36e3ac07677dafb87a868
Sudha Redroutu <156336019+Sudha-Redrouthu@users.noreply.github.com>
2025-07-15 16:59:21 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" { default = "rg-aks-demo" }
+variable "location" { default = "East US" }
+variable "aks_name" { default = "aks-demo" }
+variable "acr_name" { default = "acrdemo12345" }
